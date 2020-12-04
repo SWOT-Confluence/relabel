898109b2ea329ffae027ae5c65588d19e8417b58
Nikki Tebaldi <ntebaldi@umass.edu>
2020-12-04 09:41:47 -0500

    Modified to output only one netcdf.

--- relabel.py
+++ relabel.py
@@ -10,9 +10,10 @@ class Relabel:
 		"""Track datasets and group objects."""
 
 		self.orig_dataset = None
-		self.swot_dataset_reach = None
-		self.swot_dataset_node = None
-		self.sword_dataset = None
+		self.new_dataset = None
+		self.sword = None
+		self.swot_reach = None
+		self.swot_node = None
 
 	def relabel(self):
 		nc_files = self.get_nc_files();
@@ -29,12 +30,19 @@ class Relabel:
 		# Define datasets for original, swot, and sword data
 		self.define_datasets(file)
 
-		# Build swot datasets
-		self.build_swot_reach()
-		self.build_swot_node()
+		# Create dimensions and coordinate variables
+		self.create_dims_coords()
 
-		# Build sword dataset
-		self.build_sword()
+		# Build groups
+		self.build_groups()
+
+		# Build swot variables (reach and node)
+		self.create_reach_vars()
+		self.create_xs_vars()
+
+		# Build sword variables
+		self.create_sword_vars()
+
 	
 	def define_datasets(self, file):
 		"""Retrieve original dataset and define SWOT and SWORD of Science datasets."""
@@ -42,44 +50,45 @@ class Relabel:
 		# Retrieve original dataset 
 		self.orig_dataset = Dataset(file, "r", format="NETCDF4")
 
-		# Define podaac/SWOT dataset reach
-		swot_file = Path.cwd() / 'swot_reach' / file.name
-		self.swot_dataset_reach = Dataset(swot_file, "w", format="NETCDF4")
-
-		# Define podaac/SWOT dataset node
-		swot_file = Path.cwd() / 'swot_node' / file.name
-		self.swot_dataset_node = Dataset(swot_file, "w", format="NETCDF4")
-
-		# Define SWORD of Science dataset
-		sword_file = Path.cwd() / 'sword' / file.name
-		self.sword_dataset = Dataset(sword_file, "w", format="NETCDF4")
-
-	def build_swot_reach(self):
-		"""Build SWOT NetCDF reach files from original dataset."""
+		# Define new data set that contains: swot reach and node and sword data
+		new_file = Path.cwd() / 'output' / file.name
+		self.new_dataset = Dataset(new_file, "w", format="NETCDF4")
 
 		# Set title
-		self.swot_dataset_reach.title = self.orig_dataset.title
+		self.new_dataset.title = self.orig_dataset.title
 
-		# Create dimensions
-		self.swot_dataset_reach.createDimension("nt", 
-			self.orig_dataset.dimensions["Time steps"].size)
-		self.swot_dataset_reach.createDimension("nx", 
-			self.orig_dataset.dimensions["Reach"].size)
+	def create_dims_coords(self):
+		'''Create dimensions and coordinate variables for each dimension.'''
 		
-		# Create reach variable(s)
-		self.create_reach_vars()
+		# nt and nx dimensions
+		self.new_dataset.createDimension("nt", 
+			self.orig_dataset.dimensions["Time steps"].size)
+		self.new_dataset.createDimension("nx", 
+			self.orig_dataset.dimensions["XS_90m"].size)
+
+		# time step coordinate variable
+		nt = self.new_dataset.createVariable("nt", "i4", ("nt",))
+		nt.units = "day"
+		nt.long_name = "nt"
+		nt[:] = self.orig_dataset["Time steps"][:]
+
+		# nx coordinate variable
+		nx = self.new_dataset.createVariable("nx", "i4", ("nx",))
+		nx.units = "orthogonals"
+		nx.long_name = "nx"
+		nx[:] = self.orig_dataset["XS_90m"][:]	
+
+	def build_groups(self):
+		'''Create swot reach, node and sword groupings.'''
+		self.swot_reach = self.new_dataset.createGroup("swot_reach")
+		self.swot_node = self.new_dataset.createGroup("swot_node")
+		self.sword = self.new_dataset.createGroup("sword")
 
 	def create_reach_vars(self):
 		"""Create reach variables."""
 
-		# nt coordinate variable
-		nt = self.swot_dataset_reach.createVariable("nt", "i4", ("nt",))
-		nt.units = "day"
-		nt.long_name = "nt"
-		nt[:] = self.orig_dataset["Time steps"][:]	
-
 		# slope2
-		slope2_v = self.swot_dataset_reach.createVariable("slope2", "f8", 
+		slope2_v = self.swot_reach.createVariable("slope2", "f8", 
 			("nt",), fill_value = -999999999999)
 		slope2_v.long_name = "enhanced water surface slope with respect to geoid"
 		slope2_v.units = "m/m"
@@ -87,38 +96,11 @@ class Relabel:
 		slope2_v.valid_max = 0.1
 		slope2_v[:] = self.orig_dataset["Reach_Timeseries/S_1km"][:]
 
-	def build_swot_node(self):
-		"""Build SWOT NetCDF node files from original dataset."""
-
-		# Set new dataset title
-		self.swot_dataset_node.title = self.orig_dataset.title
-
-		# Create dimensions
-		self.swot_dataset_node.createDimension("nt", 
-			self.orig_dataset.dimensions["Time steps"].size)
-		self.swot_dataset_node.createDimension("nx", 
-			self.orig_dataset.dimensions["XS_90m"].size)
-		
-		# Create xs variables
-		self.create_xs_vars()
-
 	def create_xs_vars(self):
-		"""Create coordinate and Node group level variables."""
-
-		# time step coordinate variable
-		nt = self.swot_dataset_node.createVariable("nt", "i4", ("nt",))
-		nt.units = "day"
-		nt.long_name = "nt"
-		nt[:] = self.orig_dataset["Time steps"][:]
-
-		# nx coordinate variable
-		nx = self.swot_dataset_node.createVariable("nx", "i4", ("nx",))
-		nx.units = "orthogonals"
-		nx.long_name = "nx"
-		nx[:] = self.orig_dataset["XS_90m"][:]	
+		"""Create node variables."""
 
 		# width
-		width_v = self.swot_dataset_node.createVariable("width", "f8", ("nt", 
+		width_v = self.swot_node.createVariable("width", "f8", ("nt", 
 			"nx",), fill_value= -999999999999)
 		width_v.long_name = "node width"
 		width_v.units = "m"
@@ -127,7 +109,7 @@ class Relabel:
 		width_v[:] = self.orig_dataset["XS_Timseries/W"][:]
 
 		# wse
-		wse_v = self.swot_dataset_node.createVariable("wse", "f8", ("nt", 
+		wse_v = self.swot_node.createVariable("wse", "f8", ("nt", 
 			"nx",), fill_value= -999999999999)
 		wse_v.long_name = "water surface elevation with respect to the geoid"
 		wse_v.units = "m"
@@ -136,7 +118,7 @@ class Relabel:
 		wse_v[:] = self.orig_dataset["XS_Timseries/H_1km"][:]
 
 		# d_x_area
-		dxa_v = self.swot_dataset_node.createVariable("d_x_area", "f8", ("nt", 
+		dxa_v = self.swot_node.createVariable("d_x_area", "f8", ("nt", 
 			"nx"), fill_value = -999999999999)
 		dxa_v.long_name = "change in cross-sectional area"
 		dxa_v.units = "m^2"
@@ -151,37 +133,18 @@ class Relabel:
 		dH = h - median(h)
 		return (median(w) - w) * (dH/2)
 
-	def build_sword(self):
-		"""Build NetCDF to represent data in SWORD of Science."""
-		
-		# Set new dataset title
-		self.sword_dataset.title = self.orig_dataset.title
-
-		# Create dimensions
-		self.sword_dataset.createDimension("nt", 
-			self.orig_dataset.dimensions["Time steps"].size)		
-		
-		# Create sword variables
-		self.create_sword_vars()
-
 	def create_sword_vars(self):
 		""" Create SWORD of Science variables."""
-
-		# time step coordinate variable
-		nt = self.sword_dataset.createVariable("nt", "i4", ("nt",))
-		nt.units = "day"
-		nt.long_name = "nt"
-		nt[:] = self.orig_dataset["Time steps"][:]
 
 		# Q
 		q_v = self.orig_dataset["XS_Timseries/Q"][:]
 
 		# Qhat
-		qhat_v = self.sword_dataset.createVariable("Qhat", "f8", ("nt"), 
+		qhat_v = self.sword.createVariable("Qhat", "f8", 
 			fill_value = -999999999999)
 		qhat_v.long_name = "Qhat"
 		qhat_v.units = "m^3/s"
-		qhat_v = mean(q_v)
+		qhat_v.assignValue(mean(q_v))
 
 if __name__ == "__main__":
 	relabel = Relabel()
